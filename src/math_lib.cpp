0e5311b98d976e9a8fd079a4c077c19050c30e96
Jerry-hu <648277710@qq.com>
2024-10-20 15:46:08 +0800

    add trace msg

--- src/math_lib.cpp
+++ src/math_lib.cpp
@@ -4,14 +4,17 @@
 namespace math_lib {
 
 int add(int a, int b) {
+    std::cout<<"add"<<std::endl;
     return a + b;
 }
 
 int subtract(int a, int b) {
+    std::cout<<"subtract"<<std::endl;
     return a - b;
 }
 
 const char* get_version() {
+    std::cout<<"get_version"<<std::endl;
     return MATH_LIB_VERSION_STRING;
 }
 
